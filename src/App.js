ffaab9772e052c79c0f2e64e730a1693a19b4f20
dreyliba <libaandrey@gmail.com>
2023-03-13 15:40:45 +0800

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Router, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import routes from "./routes/routes.js";
+import Private from "./routes/Private";
+import Public from "./routes/Public";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React is Awesome!!</h1>
-    </div>
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        {routes.map((route, index) =>
+          route.auth ? (
+            <Private key={index} exact {...route} />
+          ) : (
+            <Public key={index} exact {...route} />
+          )
+        )}
+      </Switch>
+    </Router>
   );
 }
 
